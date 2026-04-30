1df8184bbb44404b84a6b1233c4d99b436ffaddc
ianoh <mutharimiian259@gmail.com>
2026-04-30 07:48:50 +0300

    Add StickyCTA component for contact options

--- components/StickyCTA.tsx
+++ components/StickyCTA.tsx
@@ -1,1 +1,13 @@
+export default function StickyCTA() {
+  return (
+    <div className="fixed bottom-0 w-full flex justify-around bg-black border-t border-gold p-3 md:hidden">
 
+      <a href="tel:+254728588005">📞 Call</a>
+
+      <a className="text-green-400" href="https://wa.me/254728588005">
+        💬 WhatsApp
+      </a>
+
+    </div>
+  );
+}
