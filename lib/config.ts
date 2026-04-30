5729fb16bfa6969670bc51ef19d9c80d64b6bf05
ianoh <mutharimiian259@gmail.com>
2026-04-30 10:59:20 +0300

    Add configuration for Three Steers Hotel

---
+++ lib/config.ts
@@ -0,0 +1,20 @@
+export const config = {
+  siteName: "Three Steers Hotel Meru",
+  domain: "https://www.threesteershotel.com/",
+
+  whatsappNumber: "254728588005",
+
+  email: "reservation@threesteershotel.com",
+
+  phone: "+254 728 588 005",
+
+  location: "Meru, Kenya",
+
+  seoKeywords: [
+    "hotel in Meru Kenya",
+    "accommodation Meru",
+    "best hotel Meru",
+    "conference venues Meru",
+    "Mt Kenya hotel",
+  ],
+};
