a7315ed87f35e2a4891164e0effae1bd94c0bc93
ianoh <mutharimiian259@gmail.com>
2026-04-30 15:38:02 +0300

    Create CallBar.tsx

---
+++ components/global/CallBar.tsx
@@ -0,0 +1,9 @@
+export default function CallBar() {
+  return (
+    <div className="hidden md:flex fixed top-0 left-0 right-0 bg-yellow-500 text-black justify-center p-2 text-sm z-50">
+
+      Call us directly for instant booking: +254 728 588 005
+
+    </div>
+  );
+}
