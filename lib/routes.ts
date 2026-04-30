3a3ec54d200893df3ab2a7c0ef6f3babfed942fe
ianoh <mutharimiian259@gmail.com>
2026-04-30 11:16:27 +0300

    Create routes.ts

---
+++ lib/routes.ts
@@ -0,0 +1,9 @@
+export const routes = [
+  { name: "Home", path: "/" },
+  { name: "Rooms", path: "/rooms" },
+  { name: "Dining", path: "/#dining" },
+  { name: "Conference", path: "/#conference" },
+  { name: "Experiences", path: "/#experiences" },
+  { name: "Blog", path: "/blog" },
+  { name: "Contact", path: "/#contact" },
+];
