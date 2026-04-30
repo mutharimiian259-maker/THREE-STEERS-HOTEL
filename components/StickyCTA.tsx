54e9d961bcc52cbcdbeeff5614993e8fc6615e48
ianoh <mutharimiian259@gmail.com>
2026-04-30 07:49:52 +0300

    Update phone numbers in StickyCTA component

--- components/StickyCTA.tsx
+++ components/StickyCTA.tsx
@@ -2,9 +2,9 @@ export default function StickyCTA() {
   return (
     <div className="fixed bottom-0 w-full flex justify-around bg-black border-t border-gold p-3 md:hidden">
 
-      <a href="tel:+254728588005">📞 Call</a>
+      <a href="tel:+254107506429">📞 Call</a>
 
-      <a className="text-green-400" href="https://wa.me/254728588005">
+      <a className="text-green-400" href="https://wa.me/254107506429">
         💬 WhatsApp
       </a>
 
