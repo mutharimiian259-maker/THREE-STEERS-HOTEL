0bfa9acdb6a75cd0a47a7c89cca5acf1c9bf9dd2
ianoh <mutharimiian259@gmail.com>
2026-04-30 22:16:12 +0300

    Update ContactInfo.tsx

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -1,39 +1,45 @@
+"use client";
+
+import { HOTEL } from "@/lib/config/hotel";
+import { buildWhatsAppLink } from "@/lib/utils/whatsapp";
+import { getPhoneLink, getEmailLink } from "@/lib/utils/contact";
+
 export default function ContactInfo() {
   return (
     <section className="p-6 bg-black text-center">
 
       {/* SEO TITLE */}
       <h2 className="text-xl font-bold text-yellow-500">
-        Contact Three Steers Hotel
+        Contact {HOTEL.name}
       </h2>
 
-      {/* EMAIL (CLICKABLE) */}
+      {/* EMAIL */}
       <a
-        href="mailto:reservation@threesteershotel.com"
+        href={getEmailLink()}
         className="block mt-2 text-gray-400 hover:text-white"
       >
-        reservation@threesteershotel.com
+        {HOTEL.contact.email}
       </a>
 
-      {/* PHONE NUMBERS */}
+      {/* PHONE */}
       <p className="text-gray-400 mt-1">
-        +254 728 588 005 / +254 735 497 772
+        {HOTEL.contact.phone}
       </p>
 
       {/* CTA BUTTONS */}
       <div className="mt-4">
 
         <a
-          href="tel:+254728588005"
+          href={getPhoneLink()}
           className="px-6 py-2 bg-yellow-500 text-black rounded-lg m-2 inline-block"
         >
           Call Now
         </a>
 
         <a
-          href={`https://wa.me/254728588005?text=${encodeURIComponent(
-            "Hello, I would like to make a booking at Three Steers Hotel Meru"
-          )}`}
+          href={buildWhatsAppLink(
+            `Hello, I would like to make a booking at ${HOTEL.name}`
+          )}
           className="px-6 py-2 bg-green-600 text-white rounded-lg m-2 inline-block"
         >
           WhatsApp
