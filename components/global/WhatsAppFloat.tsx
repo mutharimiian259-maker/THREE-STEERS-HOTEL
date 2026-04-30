a3593b1541a90e7ad3c9e249f25d1d14879daf45
ianoh <mutharimiian259@gmail.com>
2026-04-30 15:36:10 +0300

    Update WhatsAppFloat.tsx

--- components/global/WhatsAppFloat.tsx
+++ components/global/WhatsAppFloat.tsx
@@ -1,1 +1,14 @@
+"use client";
 
+const WHATSAPP_NUMBER = "254728588005";
+
+export default function WhatsAppFloat() {
+  return (
+    <a
+      href={`https://wa.me/${WHATSAPP_NUMBER}`}
+      className="fixed bottom-20 right-5 bg-green-500 text-white p-4 rounded-full shadow-lg z-50"
+    >
+      💬
+    </a>
+  );
+}
