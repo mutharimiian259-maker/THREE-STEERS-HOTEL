23f749fffca466eb3e5ca77698569f56854fde6e
ianoh <mutharimiian259@gmail.com>
2026-04-30 11:36:34 +0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,20 +1,8 @@
 export const config = {
   siteName: "Three Steers Hotel Meru",
-  domain: "https://www.threesteershotel.com/",
-
+  domain: "https://threesteershotel.com",
   whatsappNumber: "254728588005",
-
   email: "reservation@threesteershotel.com",
-
-  phone: "+254 728 588 005",
-
+  phone: "+254728588005",
   location: "Meru, Kenya",
-
-  seoKeywords: [
-    "hotel in Meru Kenya",
-    "accommodation Meru",
-    "best hotel Meru",
-    "conference venues Meru",
-    "Mt Kenya hotel",
-  ],
 };
