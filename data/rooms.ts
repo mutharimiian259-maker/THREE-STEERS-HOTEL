bb0c4288621fa683ebc09cd6cb91d963f9975385
ianoh <mutharimiian259@gmail.com>
2026-04-30 07:44:32 +0300

    Add rooms data with details and images

---
+++ data/rooms.ts
@@ -0,0 +1,18 @@
+const rooms = [
+  {
+    id: "deluxe",
+    name: "Deluxe Room",
+    price: "KSH 8,500",
+    desc: "Luxury comfort with modern amenities",
+    image: "/images/room1.jpg"
+  },
+  {
+    id: "executive",
+    name: "Executive Suite",
+    price: "KSH 12,000",
+    desc: "Premium suite with Mt Kenya views",
+    image: "/images/room2.jpg"
+  }
+];
+
+export default rooms;
