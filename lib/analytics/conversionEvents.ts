8677cb4a87534e1e104a9f7ffc46df716a8684ba
ianoh <mutharimiian259@gmail.com>
2026-05-01 13:03:58 +0300

    Remove analytics tracking functions
    Remove tracking functions for room views, WhatsApp clicks, call clicks, and booking intents.

--- lib/analytics/conversionEvents.ts
+++ lib/analytics/conversionEvents.ts
@@ -1,17 +1,1 @@
-import { trackEvent } from "./trackEvent";
 
-export function trackRoomView(roomName: string) {
-  trackEvent("room_view", { room: roomName });
-}
-
-export function trackWhatsAppClick(roomName?: string) {
-  trackEvent("whatsapp_click", { room: roomName });
-}
-
-export function trackCallClick() {
-  trackEvent("call_click");
-}
-
-export function trackBookingIntent(roomName?: string) {
-  trackEvent("booking_intent", { room: roomName });
-}
