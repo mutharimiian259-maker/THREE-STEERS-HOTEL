ef570dbcca48ce372e301f7bf3d79e6f5ae065bd
ianoh <mutharimiian259@gmail.com>
2026-05-01 17:27:00 +0300

    Update whatsapp.ts

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,7 +1,7 @@
-import { HOTEL } from "@/lib/config/hotel";
+import { HOTEL } from "@/lib/config";
 
 export function buildWhatsAppLink(message: string) {
   const encoded = encodeURIComponent(message);
 
-  return `https://wa.me/${HOTEL.contact.whatsapp}?text=${encoded}`;
+  return `https://wa.me/${HOTEL.contact.phone.whatsapp}?text=${encoded}`;
 }
