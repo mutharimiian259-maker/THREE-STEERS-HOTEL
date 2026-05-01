bbd1b411b4ff0467e72e5f3c9e05bf26cd8210e1
ianoh <mutharimiian259@gmail.com>
2026-05-01 17:31:08 +0300

    Update contact.ts

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,7 +1,7 @@
-import { HOTEL } from "@/lib/config/hotel";
+import { HOTEL } from "@/lib/config";
 
 export function getPhoneLink() {
-  return `tel:${HOTEL.contact.phone}`;
+  return `tel:${HOTEL.contact.phone.primary}`;
 }
 
 export function getEmailLink() {
