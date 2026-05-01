3b17a1fbf42b719ee46aaa3f648c23c35d084ab6
ianoh <mutharimiian259@gmail.com>
2026-05-01 13:11:57 +0300

    Update routes.ts

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,9 +1,15 @@
-export const routes = [
-  { name: "Home", path: "/" },
-  { name: "Rooms", path: "/rooms" },
-  { name: "Dining", path: "/#dining" },
-  { name: "Conference", path: "/#conference" },
-  { name: "Experiences", path: "/#experiences" },
-  { name: "Blog", path: "/blog" },
-  { name: "Contact", path: "/#contact" },
+export type Route = {
+  name: string;
+  path: string;
+  type: "page" | "section";
+};
+
+export const routes: Route[] = [
+  { name: "Home", path: "/", type: "page" },
+  { name: "Rooms", path: "/rooms", type: "page" },
+  { name: "Dining", path: "/#dining", type: "section" },
+  { name: "Conference", path: "/#conference", type: "section" },
+  { name: "Experiences", path: "/#experiences", type: "section" },
+  { name: "Blog", path: "/blog", type: "page" },
+  { name: "Contact", path: "/#contact", type: "section" },
 ];
