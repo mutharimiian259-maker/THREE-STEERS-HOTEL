85d8855848b2819335497e3ae89f6b36de87da4d
ianoh <mutharimiian259@gmail.com>
2026-05-03 16:58:32 +0300

    Update CallBar.tsx

--- components/global/CallBar.tsx
+++ components/global/CallBar.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { HOTEL } from "@/lib/config/hotel";
+import { HOTEL } from "@/lib/config";
 
 export default function CallBar() {
   const phone = HOTEL.contact.phone.primary.replace(/\s/g, "");
 
   return (
     <div className="hidden md:flex fixed top-0 left-0 right-0 bg-yellow-500 text-black justify-center p-2 text-sm z-50">
-
       Call us directly for instant booking:{" "}
       <a href={`tel:${phone}`} className="font-semibold underline">
         {HOTEL.contact.phone.primary}
       </a>
-
     </div>
   );
 }
