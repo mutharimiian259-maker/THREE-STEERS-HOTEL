9f4fbe0f9ffd617a36bcc55e880af7fd8f40a6b9
ianoh <mutharimiian259@gmail.com>
2026-05-07 11:20:28 +0300

    Update bootstrap.ts

--- lib/adapters/bootstrap.ts
+++ lib/adapters/bootstrap.ts
@@ -1,13 +1,20 @@
-import { registerAdapter } from "./router";
+import { registerAdapter, resetAdapters } from "@/lib/core/router";
 
 import { GAAdapter } from "@/lib/adapters/gaAdapter";
 import { LocalStorageAdapter } from "@/lib/adapters/localStorageAdapter";
 import { FunnelAdapter } from "@/lib/adapters/funnelAdapter";
 import { LeadAdapter } from "@/lib/adapters/leadAdapter";
 
+let initialized = false;
+
 export function initAnalytics() {
-  registerAdapter(GAAdapter);
-  registerAdapter(LocalStorageAdapter);
-  registerAdapter(FunnelAdapter);
-  registerAdapter(LeadAdapter);
+  if (initialized) return;
+  initialized = true;
+
+  resetAdapters();
+
+  registerAdapter(new GAAdapter());
+  registerAdapter(new LocalStorageAdapter());
+  registerAdapter(new FunnelAdapter());
+  registerAdapter(new LeadAdapter());
 }
