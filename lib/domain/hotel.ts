3aa7ecbdf4a32fb8e7d53af3a9bcd4a642375487
ianoh <mutharimiian259@gmail.com>
2026-05-07 09:04:08 +0300

    Create hotel.ts

---
+++ lib/domain/hotel.ts
@@ -0,0 +1,6 @@
+import { HOTEL } from "@/lib/config";
+
+export const getDomain = () => HOTEL.domain.primary;
+export const getSEO = () => HOTEL.seo;
+export const getIdentity = () => HOTEL.identity;
+export const getLocation = () => HOTEL.location;
